725eefb5029161d914fc3de51bdf2786093f1455
Aaron Hunter <aaron@voxable.io>
2017-06-28 13:04:46 -0500

    Yield to Rubocop indentation desires

--- lib/expando/api_ai/objects/intent.rb
+++ lib/expando/api_ai/objects/intent.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Expando::ApiAi::Objects
   # Initialized with a hash representing an existing API.ai intent, and the path
   # to an Expando file for that intent, generates the JSON for a new version of
@@ -47,101 +49,101 @@ module Expando::ApiAi::Objects
 
     private
 
-      # Generate new user utterances based on the Expando source for this intent.
-      #
-      # @return [Array<Hash>] The new `userSays` attribute.
-      def processed_utterances
-        @processed_utterances ||= Expando::Expander.expand! @source_file.lines
-      end
+    # Generate new user utterances based on the Expando source for this intent.
+    #
+    # @return [Array<Hash>] The new `userSays` attribute.
+    def processed_utterances
+      @processed_utterances ||= Expando::Expander.expand! @source_file.lines
+    end
 
-      # TODO: High- document and test
-      def user_says_value(existing_params)
-        additional_params = Set.new(existing_params)
+    # TODO: High- document and test
+    def user_says_value(existing_params)
+      additional_params = Set.new(existing_params)
 
-        new_user_says = processed_utterances.collect do |utterance|
-          # If an entity is referenced on this line...
-          if utterance.match(Expando::Tokens::ENTITY_REF_MATCHER)
-            template = utterance.dup
-            data = []
+      new_user_says = processed_utterances.collect do |utterance|
+        # If an entity is referenced on this line...
+        if utterance.match(Expando::Tokens::ENTITY_REF_MATCHER)
+          template = utterance.dup
+          data = []
 
-            # For every matching entity reference...
-            utterance.scan(Expando::Tokens::ENTITY_REF_MATCHER).each do |entity_reference|
-              entity_name, parameter_name = entity_reference
+          # For every matching entity reference...
+          utterance.scan(Expando::Tokens::ENTITY_REF_MATCHER).each do |entity_reference|
+            entity_name, parameter_name = entity_reference
 
-              additional_params << {
-                dataType: "@#{entity_name}",
-                name: parameter_name,
-                value: "$#{parameter_name}",
-                isList: false
-              }
+            additional_params << {
+              dataType: "@#{entity_name}",
+              name: parameter_name,
+              value: "$#{parameter_name}",
+              isList: false
+            }
 
-              # Find a matching entity file.
-              # TODO: High - throw an error if none.
-              entity_file = @entity_files.select { |entity_file| entity_file.entity_name == entity_name }.first
-              # Grab a random canonical value for the entity.
-              example_entity_value = entity_file.random_canonical_value
+            # Find a matching entity file.
+            # TODO: High - throw an error if none.
+            entity_file = @entity_files.select { |entity_file| entity_file.entity_name == entity_name }.first
+            # Grab a random canonical value for the entity.
+            example_entity_value = entity_file.random_canonical_value
 
-              # Add data entries.
-              data << { text: template.match(Expando::Tokens::UNTIL_ENTITY_REF_MATCHER)[0] }
-              data << {
-                text: example_entity_value,
-                alias: parameter_name,
-                meta: "@#{entity_name}",
-                userDefined: true
-              }
-
-              # Remove the processed portions from the template string
-              template.sub!(Expando::Tokens::UNTIL_ENTITY_REF_MATCHER, '')
-              template.sub!(Expando::Tokens::ENTITY_REF_MATCHER, '')
-            end
-
-            # Add everything that remains.
+            # Add data entries.
+            data << { text: template.match(Expando::Tokens::UNTIL_ENTITY_REF_MATCHER)[0] }
             data << {
-              text: template
+              text: example_entity_value,
+              alias: parameter_name,
+              meta: "@#{entity_name}",
+              userDefined: true
             }
 
-            {
-              data: data,
-              isTemplate: false
-            }
-          else
-            {
-              data: [
-                text: utterance
-              ],
-              # TODO: Make this an option
-              isTemplate: false
-            }
+            # Remove the processed portions from the template string
+            template.sub!(Expando::Tokens::UNTIL_ENTITY_REF_MATCHER, '')
+            template.sub!(Expando::Tokens::ENTITY_REF_MATCHER, '')
           end
+
+          # Add everything that remains.
+          data << {
+            text: template
+          }
+
+          {
+            data: data,
+            isTemplate: false
+          }
+        else
+          {
+            data: [
+              text: utterance
+            ],
+            # TODO: Make this an option
+            isTemplate: false
+          }
         end
-
-        [new_user_says, additional_params.to_a]
       end
 
-      # Generate new responses for this intent based on the Expando responses source.
-      #
-      # @return [Array<String>] The new responses.
-      def responses
-        return false unless @responses_file
+      [new_user_says, additional_params.to_a]
+    end
 
-        Expando::Expander.expand! @responses_file.lines
-      end
+    # Generate new responses for this intent based on the Expando responses source.
+    #
+    # @return [Array<String>] The new responses.
+    def responses
+      return false unless @responses_file
 
-      # Fetch the existing intent with this name on API.ai.
-      #
-      # @return [Hash] The current version of the intent object on API.ai.
-      def current_version
-        @@intents ||= @api_client.get_intents_request
+      Expando::Expander.expand! @responses_file.lines
+    end
 
-        matching_intent = @@intents.select { |intent| intent[:name] == @source_file.intent_name }
+    # Fetch the existing intent with this name on API.ai.
+    #
+    # @return [Hash] The current version of the intent object on API.ai.
+    def current_version
+      @@intents ||= @api_client.get_intents_request
 
-        # TODO: needs an exception class
-        raise "There is no intent named #{@source_file.intent_name}" if matching_intent.empty?
+      matching_intent = @@intents.select { |intent| intent[:name] == @source_file.intent_name }
 
-        intent_id = matching_intent.first[:id]
+      # TODO: needs an exception class
+      raise "There is no intent named #{@source_file.intent_name}" if matching_intent.empty?
 
-        Expando::Logger.log "Fetching latest version of #{@source_file.intent_name} intent"
-        @api_client.get_intent_request(intent_id)
-      end
+      intent_id = matching_intent.first[:id]
+
+      Expando::Logger.log "Fetching latest version of #{@source_file.intent_name} intent"
+      @api_client.get_intent_request(intent_id)
+    end
   end
 end
